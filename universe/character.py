5d4024fc852f13aad7ec55cf70cb8b2b32f924cc
Jules-castres <jules.castres@efrei.net>
2025-12-02 16:02:04 +0100

    modif ask text

---
+++ universe/character.py
@@ -0,0 +1,19 @@
+def init_character(last_name,first_name,attributes):
+    """
+    The def will return your character information
+    :param last_name: string
+    :param first_name: string
+    :param attributes: dictionary
+    :return: dictionary
+    """
+    character={"Last Name":last_name,
+    "First Name": first_name,
+    "Money":100,
+    "Inventory": [],
+    "Spells": [],
+    "Attributes": attributes }
+    return character
+
+def modify_money(characters,amount):
+    characters[3]=amount
+    return characters[3]
