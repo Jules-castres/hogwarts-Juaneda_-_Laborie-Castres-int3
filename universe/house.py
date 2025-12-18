6187f62ac802e1028e8a6120d9ac9a35365af409
Jules-castres <jules.castres@efrei.net>
2025-12-18 17:25:53 +0100

    modif assign_house max_point tested and approved

--- universe/house.py
+++ universe/house.py
@@ -57,11 +57,12 @@ def assign_house(character, questions):
         a = input_utils.ask_choice(question[0],question[1])
         update_house_point(houses, question[2][a-1], 3)
 
-    print("Summary of scores:")
+    print("\nSummary of scores:")
     max_point = 0
     name_house = ""
     for name in houses.keys():
         print(f'{name}: {houses[name]} points')
         if houses[name] > max_point:
             name_house = name
+            max_point = houses[name]
     return name_house
